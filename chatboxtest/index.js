f7561e40236a6186e99e511f34f0ddef16bd611c
nbdaaron <aaron.kau@me.com>
2016-12-06 21:52:03 -0500

    Initial Commit with Chatbox testing and Canvas Testing

---
+++ chatboxtest/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var currID = 1;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/test', function(req, res){ 
+	res.sendFile(__dirname+'/test.html');
+});
+
+app.use(express.static('static'))
+
+io.on('connection', function(socket){
+	var id = currID++;
+	console.log("A user connected with ID " + id);
+	socket.on('disconnect', function() {
+  		console.log("User " + id + " disconnected.");
+  		io.emit('chat message', "User " + id + " LEFT!!!!!");
+  	});
+  	socket.on('chat message', function(msg) {
+  		console.log(msg);
+  		io.emit('chat message', "User " + id + ": " + msg);
+  	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
