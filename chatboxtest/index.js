e7a1bd1fc011c102470f0ede0d66300987923891
nbdaaron <aaron.kau@me.com>
2016-12-15 02:06:37 -0500

    C4 Finalized, Started Socket Testing

--- chatboxtest/index.js
+++ chatboxtest/index.js
@@ -2,38 +2,53 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var C4 = require('./C4');
 
 var currID = 1;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/C4.html');
+
 });
 
 app.get('/test', function(req, res){ 
 	res.sendFile(__dirname+'/index.html');
+
 });
 
 app.get('/c4', function(req, res) {
-  res.sendFile(__dirname+'/C4.html');
+  C4.handle(req, res, io);
+  //res.sendFile(__dirname+'/C4.html');
 })
 
 app.use(express.static('static'))
 
-io.on('connection', function(socket){
-	var id = currID++;
-	console.log("A user connected with ID " + id);
-	socket.on('disconnect', function() {
-  		console.log("User " + id + " disconnected.");
-  		io.emit('chat message', "User " + id + " LEFT!!!!!");
-  	});
-  	socket.on('chat message', function(msg) {
-  		if (msg == "fuck") {
-  			msg = "i love ponies";
-  		}
-  		console.log(msg);
-  		io.emit('chat message', "User " + id + ": " + msg);
-  	});
+  var main = io.of('/main');
+  main.on('connection', function(socket){
+  var id;
+  //console.log("A user connected with ID " + id);
+  socket.on('jchat', function() {
+    id = currID++;
+  });
+  socket.on('disconnect', function() {
+      console.log("User " + id + " disconnected.");
+      main.emit('chat message', "User " + id + " LEFT!!!!!");
+    });
+    socket.on('chat message', function(msg) {
+      if (msg == "fuck") {
+        msg = "i love ponies";
+      }
+      console.log(msg);
+      main.emit('chat message', "User " + id + ": " + msg);
+    });
+
 });
+
+  var C4IO = io.of('/C4');
+  C4IO.on('connection', C4.on);
+
+
+
 
 var port = process.env.PORT || 3000
 http.listen(port, function(){
